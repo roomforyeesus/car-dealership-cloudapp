60212c98c34b9fb889e9f89aa71747185c339f6c
roomforyeesus <hantothelei@protonmail.com>
2022-06-25 22:51:17 +0000

    some static pages

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,21 +8,22 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
+    path(route='', view=views.djangoTemplate, name='static_django_template'),
 
     # path for about view
-
+    path(route='about/', view=views.about, name='about'),
     # path for contact us view
-
+    path(route='contact/', view=views.contact, name='contact'),
     # path for registration
 
     # path for login
 
     # path for logout
-
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
 
     # path for add a review view
 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
+ + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
