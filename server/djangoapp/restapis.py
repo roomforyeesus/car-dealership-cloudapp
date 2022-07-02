b74458d8ae8045e5b85670af7bf9841e79f232c2
roomforyeesus <89024752+roomforyeesus@users.noreply.github.com>
2022-07-01 19:31:55 -0700

    registration update

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -27,7 +27,7 @@ def get_request(url, **kwargs):
                                     params=kwargs)
     except:
         # If any error occurs
-        print("Network exception occurred")
+        print("Network exception occurred. restapi.py line 30")
 
     status_code = response.status_code
     print("With status {} ".format(status_code))
